d41e85cc34d1e26d52de6ad6350b0fe33b49adae
imadbenmadi <imadbenmadi891@gmail.com>
2025-02-10 16:44:48 +0100

    Update Ticket model: add unique index on userId and establish relationship with Meetings_schedule

--- Server/models/models/Ticket.js
+++ Server/models/models/Ticket.js
@@ -1,76 +1,87 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = require("../../database/Mysql.database");
 const { Meetings_schedule } = require("./Meetings_schedule");
-const Ticket = sequelize.define("Ticket", {
-    userId: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-    },
-    firstName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    lastName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            isEmail: true,
+const Ticket = sequelize.define(
+    "Ticket",
+    {
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true,
+            },
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        telephone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        service_type: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        status: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        projectId: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        Deadline: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+        init_payment_amount: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: true,
+        },
+        init_payment_date: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+        final_payment_date: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+        init_payment_status: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        init_payment_method: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        init_payment_Image: {
+            type: DataTypes.TEXT,
+            allowNull: true,
         },
     },
-    description: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-    },
-    telephone: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    service_type: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    status: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-
-    projectId: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-    },
-    Deadline: {
-        type: DataTypes.DATE,
-        allowNull: true,
-    },
-    init_payment_amount: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: true,
-    },
-    init_payment_date: {
-        type: DataTypes.DATE,
-        allowNull: true,
-    },
-    final_payment_date: {
-        type: DataTypes.DATE,
-        allowNull: true,
-    },
-    init_payment_status: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-    init_payment_method: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-    init_payment_Image: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-    },
-});
+    {
+        indexes: [
+            {
+                unique: true,
+                fields: ["userId"],
+            },
+        ],
+    }
+);
 
 Ticket.HasMany(Meetings_schedule);
 Meetings_schedule.belongsTo(Ticket);
