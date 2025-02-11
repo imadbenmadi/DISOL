9c0ba864de95439fe161e356635632e7d7858e4d
imadbenmadi <imadbenmadi891@gmail.com>
2025-02-11 17:52:33 +0100

    Update Payment model: add ticket_id field, projectId field, and new index on entity_id, entity_type, and status

--- Server/models/models/Payment.js
+++ Server/models/models/Payment.js
@@ -3,37 +3,56 @@ const sequelize = require("../../../database/Mysql.database");
 const { Ticket } = require("./Ticket");
 const { Project } = require("./Project");
 
-const Payment = sequelize.define("Payment", {
-    entity_type: {
-        type: DataTypes.ENUM("ticket", "project"),
-        allowNull: false,
+const Payment = sequelize.define(
+    "Payment",
+    {
+        entity_type: {
+            type: DataTypes.ENUM("ticket", "project"),
+            allowNull: false,
+        },
+        projectId: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        ticket_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+
+        project_version: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        entity_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        payment_amount: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false,
+        },
+        payment_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        status: {
+            type: DataTypes.ENUM("pending", "paid", "failed"),
+            allowNull: false,
+            defaultValue: "pending",
+        },
+        payment_method: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
     },
-    project_version: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-    },
-    entity_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    },
-    payment_amount: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false,
-    },
-    payment_date: {
-        type: DataTypes.DATE,
-        allowNull: false,
-    },
-    status: {
-        type: DataTypes.ENUM("pending", "paid", "failed"),
-        allowNull: false,
-        defaultValue: "pending",
-    },
-    payment_method: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-});
+    {
+        indexes: [
+            {
+                fields: ["entity_id", "entity_type", "status"],
+            },
+        ],
+    }
+);
 
 Ticket.hasMany(Payment, {
     foreignKey: "entity_id",
